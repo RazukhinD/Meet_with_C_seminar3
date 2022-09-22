df59f68dbd6289101aa8647eb18c2dd98ae891f9
Dmitrii <razuhindm@gmail.com>
2022-09-22 22:43:52 +0300

    Домашняя работа Задача 21

---
+++ Task21/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите координату X первой точки: ");
+double Xa = Convert.ToDouble(ReadLine()!);
+Write("Введите координату Y первой точки: ");
+double Ya = Convert.ToDouble(ReadLine()!);
+Write("Введите координату Z первой точки: ");
+double Za = Convert.ToDouble(ReadLine()!);
+Write("Введите координату X второй точки: ");
+double Xb = Convert.ToDouble(ReadLine()!);
+Write("Введите координату Y второй точки: ");
+double Yb = Convert.ToDouble(ReadLine()!);
+Write("Введите координату Z первой точки: ");
+double Zb = Convert.ToDouble(ReadLine()!);
+
+double lenght = Math.Sqrt(Math.Pow(Xa-Xb,2)+Math.Pow(Ya-Yb,2)+Math.Pow(Za-Zb,2));
+
+Write($"Длина заданного отрезка в пространстве {lenght:f2}");
