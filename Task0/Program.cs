b86532513bfe2888cd1323a399ba233c82041a95
Dmitrii <razuhindm@gmail.com>
2022-09-22 20:32:50 +0300

    Первая задача 3 Семинара

---
+++ Task0/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите координату X: ");
+int x = Convert.ToInt32(ReadLine()!);
+Write("Введите координату Y: ");
+int y = Convert.ToInt32(ReadLine()!);
+if (x>0 && y>0)
+{
+    WriteLine("1 четверть");
+    return;
+}
+if (x<0 && y>0)
+{
+    WriteLine("2 четверть");
+    return;
+}
+if (x<0 && y<0)
+{
+    WriteLine("3 четверть");
+    return;
+}
+if (x>0 && y<0)
+{
+    WriteLine("4 четверть");
+    return;
+}
