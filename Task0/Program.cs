5cf20c970fdfc3548b84af47343548fd8a66db29
Dmitrii <razuhindm@gmail.com>
2022-09-22 20:44:34 +0300

    Первая задача добавил проверку  на ноль 3 Семинара

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -5,23 +5,24 @@ Write("Введите координату X: ");
 int x = Convert.ToInt32(ReadLine()!);
 Write("Введите координату Y: ");
 int y = Convert.ToInt32(ReadLine()!);
+if (x==0 || y==0)
+{
+    WriteLine("Error");  
+    return;
+}
 if (x>0 && y>0)
 {
-    WriteLine("1 четверть");
-    return;
+    WriteLine("1 четверть");  
 }
 if (x<0 && y>0)
 {
     WriteLine("2 четверть");
-    return;
 }
 if (x<0 && y<0)
 {
     WriteLine("3 четверть");
-    return;
 }
 if (x>0 && y<0)
 {
     WriteLine("4 четверть");
-    return;
 }
