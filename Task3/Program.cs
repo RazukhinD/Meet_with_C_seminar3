184a513bc1a93c9b1aa72f27015905311b3730a4
Dmitrii <razuhindm@gmail.com>
2022-09-22 21:31:32 +0300

    Четвертая задача третьего семинара

---
+++ Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите номер дня недели: ");
+int num1 = Convert.ToInt32(ReadLine()!);
+
+switch (num1)
+{
+    case 1:
+    {
+        Write("Понедельник");
+        break;
+    }
+
+    case 2:
+    {
+        WriteLine("Вторник");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("Среда");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("Четверг");
+        break;
+    }
+    case 5:
+    {
+        WriteLine("Пятница");
+        break;
+    }
+    case 6:
+    {
+        WriteLine("Суббота");
+        break;
+    }
+    case 7:
+    {
+        WriteLine("Воскресение");
+        break;
+    }
+    default:
+    {
+        Write("Введи правильный день недели");
+        break;
+    }
+}
