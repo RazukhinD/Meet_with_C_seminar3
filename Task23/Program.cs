aa34d3428f0bf632c86df0c1127b37bd677651a2
Dmitrii <razuhindm@gmail.com>
2022-09-22 22:45:53 +0300

    Домашняя работа Задача 23

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введи первое число: ");
+int number = int.Parse(ReadLine()!);
+
+if (number<0)
+{
+    Write("Введи число больше 0");
+    return;
+}
+int i=1;
+while (i<=number)
+{
+    Write($" {Math.Pow(i,3)}");
+    i++;
+}
